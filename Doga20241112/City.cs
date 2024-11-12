ef24a4b68d8a8f35d30a59d60d49e81c5f8247ad
Ny19KozmaL <Ny19KozmaL@202-08>
2024-11-12 13:58:40 +0100

    Add project files.

---
+++ Doga20241112/City.cs
@@ -0,0 +1,18 @@
+﻿namespace Doga20241112;
+
+internal class City
+{
+    public string Name { get; set; }
+    public string Country { get; set; }
+    public double Population { get; set; }
+
+    public City(string row)
+    {
+        var d = row.Split(';');
+        Name = d[0];
+        Country = d[1];
+        Population = double.Parse(d[2]);
+    }
+
+    public override string? ToString() => $"{Name} {Country} {Population:F2}";
+}
---
+++ Doga20241112/Program.cs
@@ -0,0 +1,27 @@
+﻿using Doga20241112;
+
+List<City> cities = [];
+
+using StreamReader reader = new(@"../../../src/varosok.csv");
+reader.ReadLine();
+while (!reader.EndOfStream) cities.Add(new(reader.ReadLine()));
+
+Console.WriteLine($"A városok lista hossza: {cities.Count}");
+
+Console.WriteLine($"{cities.Sum(x => x.Population)} millió fő él összesen kínai nagyvárosokban.");
+
+var f2 = cities.Where(x => x.Country == "India").Average(x => x.Population);
+Console.WriteLine($"Indiai nagyvárosok átlaglélekszáma: {f2}");
+
+var f3 = cities.MaxBy(x => x.Population);
+Console.WriteLine($"Ez a város a legnépesebb: {f3.Name}");
+
+var f4 = cities.Where(x => x.Population >= 20).OrderBy(x => x.Population);
+foreach (var item in f4)
+    Console.WriteLine(item);
+
+var f5 = cities.GroupBy(v => v.Country).Where(g => g.Count() > 1).Select(g => g.Key).ToList();
+Console.WriteLine($"Országok, melyek több nagyvárossal is szerepelnek: { string.Join(", ", f5)}");
+
+var f6 = cities.GroupBy(x => x.Name[0]).ToDictionary(x => x.Key, x => x.Count()).MaxBy(x => x.Value);
+Console.WriteLine($"A legtöbb város neve: {f6.Key} betűvel kezdődik.");
